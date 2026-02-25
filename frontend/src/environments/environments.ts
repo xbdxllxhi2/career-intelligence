772e5562bc439a35afdce159cdc92e8363fe8437
Abdellahi <abdallahi2002.aa@gmail.com>
2026-02-25 02:35:28 +0100

    chore: minor adjustments

--- frontend/src/environments/environments.ts
+++ frontend/src/environments/environments.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: false,
   apiUrl: 'https://career-intelligence.dev.ould-abdellahi.com/stayaway',
   keycloak: {
-    enabled: false,
-    url: 'http://localhost:8080',
-    realm: 'career-intelligence-fe',
-    clientId: 'frontend'
+    enabled: true,
+    url: 'https://auth.dev.ould-abdellahi.com/',
+    realm: 'dev-career-intelligence',
+    clientId: 'career-intelligence-fe'
   },
   googleAnalyticsId: 'G-872T4C1ENB'
 };
